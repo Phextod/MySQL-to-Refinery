6123454b376714dc5d1959fb21720227d7191aa8
Illes <feketeszabo.illes@gmail.com>
2024-04-19 17:31:52 +0200

    Change config format to JSON

---
+++ src/utility/Config.py
@@ -0,0 +1,19 @@
+# MySQL connection configuration
+import json
+import os.path
+
+
+class Config:
+    def __init__(self, config_file_path):
+        with open(config_file_path) as config_file:
+            config = json.load(config_file)
+
+            self.db_config = config['db_config']
+            self.REFINERY_JAR_PATH = os.path.abspath(config['REFINERY_JAR_PATH'])
+            self.REFINERY_CODE_PATH = os.path.abspath(config['REFINERY_CODE_PATH'])
+            self.REFINERY_RESULT_PATH = os.path.abspath(config['REFINERY_RESULT_PATH'])
+            self.INSERT_SQL_PATH = os.path.abspath(config['INSERT_SQL_PATH'])
+
+
+def load_config(config_file_path):
+    return Config(config_file_path)

6e73111200f54b62526f070249b055b4722b2a6a
Illes <feketeszabo.illes@gmail.com>
2024-04-25 20:27:53 +0200

    Handle composite keys

--- src/db_model/DBModel.py
+++ src/db_model/DBModel.py
@@ -37,15 +37,21 @@ class DBModel:
         {%- endif %}
         {% endfor %}
         //composite key constraints
-        
+        {% for table_name, keys in composite_keys.items() -%}
+        error sameKeysFor_{{table_name}}({{table_name}} {{table_name}}_object) <-> 
+            {{table_name}}_object != {{table_name}}_other,
+            {% for key in keys -%}
+            {{key}}({{table_name}}_object,ref_{{key}}),
+            {{key}}({{table_name}}_other,ref_{{key}}){% if not loop.last %},{% else %}.{% endif %}
+            {% endfor %}
+        {% endfor %}
         scope node={{ node_multiplicity_min }}..{{ node_multiplicity_max }},
            {% for name, tab in db_tables.items() -%}
            {{ name }}={{ (tab.scope_count_min * scope_multi)|int }}..{{ tab.scope_count_max }}{% if not loop.last %},{% else %}.{% endif %}
            {% endfor %}
         """))
 
-        composite_keys = self.get_composite_keys()
-        # todo create composite key constraints
+        composite_keys = self.get_reference_composite_keys()
 
         node_multiplicity_min = \
             math.ceil(sum(tab.scope_count_min for tab in self.db_tables.values()) * node_multiplicity_multiplier)
@@ -56,6 +62,7 @@ class DBModel:
             node_multiplicity_min=node_multiplicity_min,
             node_multiplicity_max=node_multiplicity_max,
             scope_multi=node_multiplicity_multiplier,
+            composite_keys=composite_keys,
         )
 
         return rendered_template
@@ -117,6 +124,9 @@ class DBModel:
         # Write INSERT statements
         for table_name, table in self.db_tables.items():
             attribute_names = [a.name for a in table.attributes]
+            has_self_reference = bool([r for r in table.relations if r.target_table_name == r.origin_table_name])
+            if has_self_reference:
+                insert_sql += "SET FOREIGN_KEY_CHECKS=0;\n"
             insert_sql += f"INSERT INTO {table_name} ({','.join(attribute_names)}) VALUES\n"
 
             unfilled_reference_attribute_names = []
@@ -142,7 +152,10 @@ class DBModel:
             else:
                 table.fill_status = FillStatus.FILLED
 
-            insert_sql = insert_sql[:-1] + ";\n\n"
+            insert_sql = insert_sql[:-1] + ";\n"
+            if has_self_reference:
+                insert_sql += "SET FOREIGN_KEY_CHECKS=1;\n"
+            insert_sql += "\n"
 
         # Write UPDATE statements for tables that were half filled
         tables_to_update = [t for t in self.db_tables.values() if t.fill_status == FillStatus.HALF_FILLED]
@@ -177,13 +190,16 @@ class DBModel:
         insert_sql = insert_sql[:-1]
         return insert_sql
 
-    def get_composite_keys(self):
-        composite_keys = {}
+    def get_reference_composite_keys(self):
+        # if a key attribute is not a reference, then refinery can't generate (same) values for it
+        reference_composite_keys = {}
         for db_table in self.db_tables.values():
             keys = [a.name for a in db_table.attributes if a.key_type == "PRI"]
-            if len(keys) > 1:
-                composite_keys.update({db_table.name: keys})
-        return composite_keys
+            relation_attributes = [a.origin_attribute_name for a in db_table.relations]
+            reference_keys = [k for k in keys if k in relation_attributes]
+            if len(reference_keys) > 1:
+                reference_composite_keys.update({db_table.name: reference_keys})
+        return reference_composite_keys
 
 
 def generate_db_model(tables, table_relations, table_descriptions) -> DBModel:
